060d96dbae7e74a4e78508238ff2a98cdfe7e72c
kareemdaley <kareem@kareems-imac.myfiosgateway.com>
2022-02-22 22:30:10 -0500

    refractered and restructured some code and files

---
+++ CreateAPI/Models/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace CreateAPI.App
+{
+    public class Transaction
+    {
+
+        public string orderID { get; set; }
+        public int orderTotal { get; set; }
+        public string Buyer { get; set; }
+        public string Store { get; set; }
+        
+        public Transaction(string orderid, string buyer, int ordertotal , string store)
+        {
+            orderID = orderid;
+            orderTotal = ordertotal;
+            Buyer = buyer;
+            Store = store;
+        }
+
+    }
+}
+
+
--- CreateAPI/Models/User.cs
+++ CreateAPI/Models/User.cs
@@ -4,7 +4,6 @@ namespace CreateAPI.App
 	public class User
 	{
 
-		// pri by default need public to see json
 		public string Name { get; set; }
 		public string ID { get; set; }
 
